facde3008c5fd4322b306926b92ec9c4ddff3bd0
luiscadari <luiscadari01@gmail.com>
2024-12-12 08:44:50 -0300

    :tada: First Commit!

---
+++ README.md
@@ -0,0 +1,2 @@
+# working-with-forms
+ Applicação que adiciona membros de uma banda usando vue.js. Aplicação para treino
---
+++ scripts.js
@@ -0,0 +1,27 @@
+members = [
+  {
+    fname: "John",
+    lname: "Lennon",
+    instrument: "Violão",
+  },
+  {
+    fname: "George",
+    lname: "Harrison",
+    instrument: "Guitarra",
+  },
+];
+
+const handlingForms = {
+  data() {
+    return {
+      members: window.members,
+      newMember: {
+        fname: null,
+        lname: null,
+        instrument: null,
+      },
+    };
+  },
+};
+
+Vue.createApp(handlingForms).mount("#app");
